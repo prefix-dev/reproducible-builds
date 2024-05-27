040dd58e9225b9fdcc219f85f299b1b7bc0c9902
nichmor <nmorkotilo@gmail.com>
2024-05-27 14:41:57 +0300

    misc: fix template string

--- src/repror/statistics.py
+++ src/repror/statistics.py
@@ -145,8 +145,14 @@ def plot(platforms, stat_dir: Path):
 
     config = load_config()
 
+    
+    if "rattler-build" not in config:
+        rattler_tmpl_string = "Built with latest rattler-build"
+    else:
+        rattler_tmpl_string = f"Built with rattler-build {config["rattler-build"]["url"]} at commit {config["rattler-build"]["branch"]}"
+    
     build_text = f"""
-    Built with rattler-build {config["rattler-build"]["url"]} at commit {config["rattler-build"]["branch"]}
+    {rattler_tmpl_string}
 
     Built on ubunutu 22.04 and rebuild on 20.04
     """
