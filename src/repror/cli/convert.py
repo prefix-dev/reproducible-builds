f838abadd29141c730c53ed1423e51a8163d57ea
Tim de Jager <tim@prefix.dev>
2024-06-04 11:33:01 +0200

    feat: convert is now a proper cli

--- src/repror/cli/convert.py
+++ src/repror/cli/convert.py
@@ -14,28 +14,30 @@ from rich import print
 app = typer.Typer()
 
 
-def checkout_feedstock(package_name: str, dest_recipe_dir: Optional[Path]):
+def checkout_feedstock(package_name: str, dest_recipe_dir: Path, clone_dir: str | bytes, dont_copy: bool):
     """
     Check out the feedstock repository for the given package name.
     """
     repo_url = f"https://github.com/conda-forge/{package_name}-feedstock"
-    with tempfile.TemporaryDirectory() as temp_dir:
-        try:
-            git.clone_repo(repo_url, temp_dir)
-            recipe_dir = os.path.join(temp_dir, "recipe")
+    try:
+        git.clone_repo(repo_url, clone_dir)
+        recipe_dir = os.path.join(clone_dir, "recipe")
+        if not dont_copy:
             print(f"Saving to: {dest_recipe_dir}")
             shutil.copytree(recipe_dir, dest_recipe_dir, dirs_exist_ok=True)
-            return True
-        except subprocess.CalledProcessError:
-            print(f"[red bold]Failed to clone repository: {repo_url}[/red bold]")
-            return None
+
+        return recipe_dir if dont_copy else dest_recipe_dir
+
+    except subprocess.CalledProcessError:
+        print(f"[red bold]Failed to clone repository: {repo_url}[/red bold]")
+        return None
 
 
-def apply_crm_convert(dest_recipe_dir: Optional[Path], meta_yaml_path: Path):
+def apply_crm_convert(dest_recipe_dir: Path, meta_yaml_path: Path, dont_save: bool = False):
     """
     Apply the crm convert tool to the given meta.yaml file.
     """
-    if dest_recipe_dir is not None:
+    if not dont_save:
         dest_file = os.path.join(dest_recipe_dir, "recipe.yaml")
         subprocess.run(
             ["crm", "convert", meta_yaml_path, "--output", dest_file], check=False
@@ -46,12 +48,13 @@ def apply_crm_convert(dest_recipe_dir: Optional[Path], meta_yaml_path: Path):
             ["crm", "convert", meta_yaml_path], check=False
         )
 
-def generate_new_recipe(dest_recipe_dir: Path):
+
+def generate_and_save_new_recipe(dest_recipe_dir: Optional[Path], dont_save: bool = False):
     """
     Save the new recipe in the specified directory.
     """
     meta_yaml_path = os.path.join(dest_recipe_dir, "meta.yaml")
-    apply_crm_convert(dest_recipe_dir, meta_yaml_path)
+    apply_crm_convert(dest_recipe_dir, meta_yaml_path, dont_save)
 
 
 def process_packages(package_names: list[str], dont_save: bool):
@@ -66,28 +69,32 @@ def process_packages(package_names: list[str], dont_save: bool):
 
         # Create the destination director
         # If there is no destination directory, we don't save the recipe
-        dest_recipe_dir = os.path.join(base_dest_dir, "recipes", package_name) if not dont_save else None
+        dest_recipe_dir = os.path.join(base_dest_dir, "recipes", package_name)
         # And output file
         recipe_path = Path(os.path.join(dest_recipe_dir, "recipe.yaml"))
 
-        if recipe_path.exists():
+        if not dont_save and recipe_path.exists():
             print(f":boom: Recipe already exists for {package_name}")
             continue
-        print("Checking out feedstock")
-        success = checkout_feedstock(package_name, dest_recipe_dir)
-        if success:
-            print(f"Saving recipe to: {dest_recipe_dir}")
-            generate_new_recipe(dest_recipe_dir)
-        else:
-            print(f"[yellow bold]Skipping package: {package_name} due to checkout failure[/yellow bold]")
-            continue
 
+        print("Checking out feedstock")
+        with tempfile.TemporaryDirectory() as temp_dir:
+            dest_recipe_dir = checkout_feedstock(package_name, dest_recipe_dir, clone_dir=temp_dir, dont_copy=dont_save)
+            # None means error in this case
+            if dest_recipe_dir is not None:
+                generate_and_save_new_recipe(dest_recipe_dir, dont_save)
+            else:
+                print(f"[yellow bold]Skipping package: {package_name} due to checkout failure[/yellow bold]")
+                continue
+
+        # Append to the configuration file
         if not dont_save:
             # Get the relative path
             rel_path = os.path.relpath(recipe_path, base_dest_dir)
             if rel_path not in all_existing_paths:
                 config["local"].append({"path": rel_path})
 
+    # Update the configuration file
     if not dont_save:
         conf.save_config(config)
 
