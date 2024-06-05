b54ccb302efecb1106249a493d41a9b4f31ecfa8
nichmor <nmorkotilo@gmail.com>
2024-06-05 12:19:45 +0300

    misc: use another function

--- src/repror/internals/print.py
+++ src/repror/internals/print.py
@@ -16,15 +16,32 @@ def strip_emojis(text: str) -> str:
         "\U0001f900-\U0001f9ff"  # Supplemental Symbols and Pictographs
         "\U0001fa00-\U0001fa6f"  # Chess Symbols
         "\U0001fa70-\U0001faff"  # Symbols and Pictographs Extended-A
+        "\U00002500-\U00002bef"  # various symbols
         "\U00002700-\U000027bf"  # Dingbats
-        "\U0001f1e0-\U0001f1ff"  # Flags (iOS)
-        "\U00002600-\U000026ff"  # Miscellaneous Symbols
-        "\U00002b50-\U00002b55"  # Additional symbols
+        "\U0001f1e0-\U0001f1ff"  # flags (iOS)
+        "\U0001f100-\U0001f1ff"  # Enclosed Alphanumeric Supplement
+        "\U0001f200-\U0001f2ff"  # Enclosed Ideographic Supplement
+        "\U0001f300-\U0001f5ff"  # Miscellaneous Symbols and Pictographs
+        "\U0001f600-\U0001f64f"  # Emoticons
+        "\U0001f680-\U0001f6ff"  # Transport and Map Symbols
+        "\U0001f700-\U0001f77f"  # Alchemical Symbols
+        "\U0001f780-\U0001f7ff"  # Geometric Shapes Extended
+        "\U0001f800-\U0001f8ff"  # Supplemental Arrows-C
+        "\U0001f900-\U0001f9ff"  # Supplemental Symbols and Pictographs
+        "\U0001fa00-\U0001fa6f"  # Chess Symbols
+        "\U0001fa70-\U0001faff"  # Symbols and Pictographs Extended-A
+        "\U0001fb00-\U0001fbff"  # Symbols for Legacy Computing
         "]+",
         flags=re.UNICODE,
     )
 
-    return emoji_pattern.sub(r"", text)
+    # Regex to remove text-based emoji representations like :dancer:
+    text_emoji_pattern = re.compile(r":[a-z_]+:", re.UNICODE)
+
+    text = emoji_pattern.sub(r"", text)
+    text = text_emoji_pattern.sub(r"", text)
+
+    return text
 
 
 def print(text: str) -> str:
