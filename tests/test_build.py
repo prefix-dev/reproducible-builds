c3d0e17284cdbe770bd38ce2472750f7efe66166
Tim de Jager <tim@prefix.dev>
2024-06-07 14:22:31 +0200

    test: added test and fixed some errors

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,2 +1,15 @@
-def test_setup_rattler_build():
-    pass
+from typer.testing import CliRunner
+
+from repror.repror import app
+
+runner = CliRunner()
+
+
+def test_build_boltons():
+    result = runner.invoke(app, ["build-recipe", "boltons"])
+    assert result.exit_code == 0
+
+
+def test_rebuild_build_boltons():
+    result = runner.invoke(app, ["rebuild-recipe", "boltons"])
+    assert result.exit_code == 0
