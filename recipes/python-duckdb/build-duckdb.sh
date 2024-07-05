a8e321e4695a93273810d9ff3fa65dc34551b758
Tim de Jager <tim@prefix.dev>
2024-07-05 15:52:27 +0200

    feat: build duck-db now (#177)

--- recipes/python-duckdb/build-duckdb.sh
+++ recipes/python-duckdb/build-duckdb.sh
@@ -8,4 +8,4 @@ if [[ "$target_platform" == "linux-ppc64le" ]]; then
   export CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-fno-plt//g') -fplt"
 fi
 
-SETUPTOOLS_SCM_PRETEND_VERSION=1.0.0 python -m pip install --no-deps --no-build-isolation tools/pythonpkg -vv
+SETUPTOOLS_SCM_PRETEND_VERSION=1.0.0 $PYTHON -m pip install --no-deps --no-build-isolation tools/pythonpkg -vv
